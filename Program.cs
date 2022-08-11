43e0d4e597a612139eb040222ae90a239c95e2c4
mathbueeno <mathbueeno@gmail.com>
2022-08-11 11:37:15 -0300

    Mudanças

--- Program.cs
+++ Program.cs
@@ -1,15 +1,31 @@
-﻿
+﻿using System;
+using System.Threading;
+
 namespace Stopwatch
 {
     class program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Start();
+        }
+
+        static void Start()
+        {
+            int time = 10;
+            int currentTime = 0;
+
+            while (currentTime != time)
+            {
+                Console.Clear();
+                currentTime++;
+                Console.WriteLine(currentTime);
+                Thread.Sleep();
+            }
         }
 
     }
 
 }
-// See https://aka.ms/new-console-template for more information
+
 
