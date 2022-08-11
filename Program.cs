21237b861e75fb536f9cbc1727afe0550781766a
mathbueeno <mathbueeno@gmail.com>
2022-08-11 11:21:39 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿
+namespace Stopwatch
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+    }
+
+}
+// See https://aka.ms/new-console-template for more information
+
