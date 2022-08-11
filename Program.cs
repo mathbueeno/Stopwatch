3197784aa6e7edf0f80f31aa2f8bb35fb78bdbec
mathbueeno <mathbueeno@gmail.com>
2022-08-11 12:00:25 -0300

    Char

--- Program.cs
+++ Program.cs
@@ -7,12 +7,25 @@ namespace Stopwatch
     {
         static void Main(string[] args)
         {
-            Start();
+            Menu();
         }
 
-        static void Start()
+        static void Menu()
         {
-            int time = 10;
+            Console.Clear();
+            Console.WriteLine("S = Segundo => 10s = 10 segundos");
+            Console.WriteLine("M = Minuto => 1m = 1 minuto");
+            Console.WriteLine("0 = Sair");
+            Console.WriteLine("Quanto tempo deseja contar?");
+
+            Console.ReadLine();
+
+
+        }
+
+        static void Start(int time)
+        {
+
             int currentTime = 0;
 
             while (currentTime != time)
@@ -20,8 +33,12 @@ namespace Stopwatch
                 Console.Clear();
                 currentTime++;
                 Console.WriteLine(currentTime);
-                Thread.Sleep();
+                Thread.Sleep(1000);
             }
+
+            Console.Clear();
+            Console.WriteLine("Stopwatch finalizado!");
+            Thread.Sleep(2500);
         }
 
     }
